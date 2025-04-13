bce5a53be2674cad838e6ce586895f307a698228
Viktor Krupenko <viktor.krupenko@gmail.com>
2025-04-14 00:53:07 +0200

    Adjust ProductToggle component

--- src/components/products/toggle.tsx
+++ src/components/products/toggle.tsx
@@ -1,17 +1,16 @@
+"use client";
 import React from "react";
+import useProductState, { ProductsStateInterface } from "@/app/hooks/useProductIndexState";
+import ProductToggleButton from "@/components/products/button";
 
 export function ProductToggle(props: ProductsProps) {
   const { products } = props;
-
-  const { productIndexStateAction }: ProductsState = {
-    productIndexStateAction: [0, () => {}],
-  };
-
+  const { productIndexStateAction }: ProductsStateInterface = useProductState(0);
   const [productIndex, setProductIndex] = productIndexStateAction;
 
   const { nextProduct, previousProduct }: ProductsEventHandlers = {
-    nextProduct: () => {},
-    previousProduct: () => {},
+    nextProduct: (e) => { setProductIndex((prev: number) => Math.min(prev + 1, products.length - 1)) },
+    previousProduct: (e) => { setProductIndex((prev: number) => Math.max(prev - 1, 0)) }
   };
 
   if (products.length === 0) {
@@ -22,47 +21,57 @@ export function ProductToggle(props: ProductsProps) {
     );
   }
 
-  const { title, description, image, presentmentPrice } =
+  const { title, description, image, price } =
     products[productIndex];
 
+  const imageSrc: string = image?.src || 'https://placehold.co/400x400.png'; // use backup image instead
+  let formattedPrice: string = price?.trim() ? price : `From ${getLowestProductPrice(products[productIndex].variants)}`; // potential for useMemo hook
+
   return (
-    <section className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-      <h1 className="text-4xl font-bold text-center">{title}</h1>
-      <img className="w-full h-auto" src={image} />
-      <p className="text-2xl text-center">{presentmentPrice}</p>
-      <p className="text-lg text-center">{description}</p>
-      <button
-        onClick={previousProduct}
-        className="bg-green-500 text-white py-2 px-4 rounded"
-      >
-        Previous Product
-      </button>
-      <button
-        onClick={nextProduct}
-        className="bg-green-500 text-white py-2 px-4 rounded"
-      >
-        Next Product
-      </button>
+    <section className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start w-full max-w-[800px] m-auto">
+      <h1 className="text-4xl font-bold text-center w-full">{title}</h1>
+      <div className="flex justify-center items-center w-full max-w-[400px] aspect-[1/1] m-auto">
+        <img className="m-auto" src={imageSrc} alt={image?.alt || title} />
+      </div>
+      <p className="text-2xl text-center w-full">{formattedPrice}</p>
+      { description &&
+        <p className="text-lg text-center">{description}</p>
+      }
+      <div className="flex justify-between w-full">
+        <ProductToggleButton message="Previous Product" callback={previousProduct} />
+        <ProductToggleButton message="Next Product" callback={nextProduct} />
+      </div>
     </section>
   );
 }
 
+function getLowestProductPrice(variants: VariantPrice[]): string {
+  if(variants.length === 1) return variants[0].price || 'N/A';
+
+  const sortedVariants = variants.slice().sort((a,b) => Number(a.price) - Number(b.price));
+
+  return sortedVariants[0].price || 'N/A';
+}
+
+interface VariantPrice {
+  price: string;
+}
+
+interface ProductImage {
+  src: string | null;
+  alt?: string;
+}
+
 interface Product {
   title: string;
-  description: string;
-  image: string;
-  presentmentPrice: string;
+  description?: string;
+  image: ProductImage;
+  price: string;
+  variants: VariantPrice[];
 }
 
 interface ProductsProps {
   products: Product[];
-}
-
-interface ProductsState {
-  productIndexStateAction: [
-    number,
-    React.Dispatch<React.SetStateAction<number>>
-  ];
 }
 
 interface ProductsEventHandlers {
