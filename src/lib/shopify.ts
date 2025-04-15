51c9bb7f708eeeaff01197490f14df57c9336102
Oleksandr Ivasiuk <ivasiukoleksandr91@gmail.com>
2025-04-15 23:03:07 +0300

    - Add exception handling using a try-catch block.
    Note: Because Shopify plans to migrate REST APIs to GraphQL APIs — for example, the REST Admin API became a legacy API on October 1, 2024 — I will use the GraphQL Storefront API to fetch the list of products.

--- src/lib/shopify.ts
+++ src/lib/shopify.ts
@@ -3,6 +3,11 @@ const SHOPIFY_STOREFRONT_ACCESS_TOKEN = process.env.SHOPIFY_STOREFRONT_ACCESS_TO
 const SHOPIFY_API_VERSION = process.env.SHOPIFY_API_VERSION;
 
 export async function fetchProducts() {
+  if (!SHOPIFY_STORE_DOMAIN || !SHOPIFY_STOREFRONT_ACCESS_TOKEN || !SHOPIFY_API_VERSION) {
+    console.error('Could not get Shopify store credentials');
+    throw new Error('Could not get Shopify store credentials!');
+  }
+
   const endpoint = `https://${SHOPIFY_STORE_DOMAIN}/api/${SHOPIFY_API_VERSION}/graphql.json`;
 
   const query = `
@@ -32,21 +37,26 @@ export async function fetchProducts() {
     }
   `;
 
-  const response = await fetch(endpoint, {
-    method: 'POST',
-    headers: {
-      'X-Shopify-Storefront-Access-Token': SHOPIFY_STOREFRONT_ACCESS_TOKEN,
-      'Content-Type': 'application/json',
-    } as HeadersInit,
-    body: JSON.stringify({ query }),
-  });
-
-  const result = await response.json();
-
-  return result.data.products.edges.map(({ node }: any) => ({
-    title: node.title,
-    description: node.description,
-    image: node.images.edges[0]?.node.url ?? "",
-    presentmentPrice: `${node.priceRange.minVariantPrice.amount} ${node.priceRange.minVariantPrice.currencyCode}`,
-  }));
+  try {
+    const response = await fetch(endpoint, {
+      method: 'POST',
+      headers: {
+        'X-Shopify-Storefront-Access-Token': SHOPIFY_STOREFRONT_ACCESS_TOKEN,
+        'Content-Type': 'application/json',
+      } as HeadersInit,
+      body: JSON.stringify({ query }),
+    });
+  
+    const result = await response.json();
+  
+    return result.data.products.edges.map(({ node }: any) => ({
+      title: node.title,
+      description: node.description,
+      image: node.images.edges[0]?.node.url ?? "",
+      presentmentPrice: `${node.priceRange.minVariantPrice.amount} ${node.priceRange.minVariantPrice.currencyCode}`,
+    }));
+  } catch (error) {
+    console.error(error);
+    throw new Error('Could not fetch products!');
+  }
 }
